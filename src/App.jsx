e17c58667e2a07bd0655ad2b2a7c1a08fbc987b7
Elibabah <delias16.hernandez@gmail.com>
2022-01-20 20:08:29 -0600

    first button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
             <div>Hello, Elibabah!</div>
             <a className= "App-link" href = "https://reactjs.org" target= "_blank" rel= "noopener noreferrer">
             Hello World!</a> 
+            <Button variant="danger">Hola, amigos!</Button>
         </header> 
         <Footer/>
     </div>
